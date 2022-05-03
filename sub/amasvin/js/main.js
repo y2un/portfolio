72b70b0c6d6613d7b98c9f887d9729f698cf4595
y2un <dpdms2777@naver.com>
2022-05-03 11:40:47 +0900

    third upload

---
+++ sub/amasvin/js/main.js
@@ -0,0 +1,14 @@
+
+$('.mainBn').slick({
+    'dots' : true,
+    'slidesToShow' : 1,
+    'arrows' : false,
+    'autoplay' : true,
+    'autoplayspeed' : 1600
+});
+
+$('.subBn').slick({
+    'slidesToShow' : 3,
+    'nextArrow' : '.nxt',
+    'prevArrow' : '.prv'
+});
